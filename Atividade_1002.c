0da3d60f96a1d6e24d164e3f6ba8609e717e6854
Tácio Patez <taciopatez@hotmail.com>
2025-02-21 00:33:09 -0300

    final commit

--- Atividade_1002.c
+++ Atividade_1002.c
@@ -22,7 +22,13 @@
 #define LED_B 12
 #define LED_R 13
 
+#define DISPLAY_Y 63 // pixels usaveis Y
+#define DISPLAY_X 127 // pixels usaveis X
+
 static volatile bool led_pwm = true;
+
+ssd1306_t ssd; // Inicializa a estrutura do display
+bool cor = true; // Borda do display
 
 // Armazena o tempo do último evento (em microssegundos)
 static volatile uint32_t last_time = 0; 
@@ -36,6 +42,11 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
         if(gpio == JOYB_PIN){
             last_time = current_time;
             gpio_put(LED_G, !gpio_get(LED_G));
+            cor = !cor;
+            // Atualiza o conteúdo do display com animações
+            ssd1306_fill(&ssd, !cor); // Limpa o display
+            ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+            ssd1306_send_data(&ssd); // Atualiza o display
         } else {
             led_pwm = !led_pwm;
             pwm_set_gpio_level(LED_R, 0);
@@ -73,19 +84,58 @@ int main()
     gpio_set_irq_enabled_with_callback(JOYB_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA); // Pull up the data line
+    gpio_pull_up(I2C_SCL); // Pull up the clock line
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_rect(&ssd, 3, 3, 63, 34, cor, !cor); // Desenha um retângulo
+    ssd1306_draw_char(&ssd, 'O', 63, 31);
+    ssd1306_send_data(&ssd);
+
     uint16_t vrx_value = 0;
     uint16_t vry_value = 0;
+
+    uint16_t point_x = 63;
+    uint16_t point_y = 31;
 
     while (true) {
         if(led_pwm){
             adc_select_input(0);
-            vrx_value = abs(adc_read()-2047)*2;
+            vrx_value = adc_read();
             adc_select_input(1);
-            vry_value = abs(adc_read()-2047)*2;
-            pwm_set_gpio_level(LED_R, vrx_value);
-            pwm_set_gpio_level(LED_B, vry_value);
+            vry_value = adc_read();
+            pwm_set_gpio_level(LED_R, abs(vrx_value-2047)*2);
+            pwm_set_gpio_level(LED_B, abs(vry_value-2047)*2);
+            
+            point_x = DISPLAY_X - 0.031 * vrx_value;
+            point_y = DISPLAY_Y - 0.014 * vry_value;
+
+            // afastar das bordas
+            if(point_x > 100) {
+                point_x -= 10;
+            } else if(point_x < 20) {
+                point_x += 10;
+            }
+            if(point_y > 50) {
+                point_y -= 10;
+            } else if(point_y < 20) {
+                point_y += 10;
+            }
+
+            ssd1306_fill(&ssd, !cor);
+            ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor);
+            ssd1306_draw_char(&ssd, 'O', point_x, point_y);
+            ssd1306_send_data(&ssd);
         }
-        
         sleep_ms(1000);
     }
 }
